1e323867d08160b4c5233c2ee000c4d8aaf423b1
Pavan Rao <srkantarao.p@husky.neu.edu>
2020-01-21 19:15:15 -0500

    Added Iterative cinary search

---
+++ IterativeBinarySearch.java
@@ -0,0 +1,55 @@
+/**
+ * Minimum time complexity: O(1)
+ * Maximum time complexity: O(logn)
+ */
+
+class IterativeBinarySearch {
+
+    private static int index, key;
+
+    static int BinarySearch(int[] array, int n, int key) {
+
+        int low = 0;
+        int high = n-1;
+        int mid;
+
+        while(low <= high) {
+            mid = (low + high) / 2;
+            if(key == array[mid]) {
+                return mid;
+            }
+            if(key < array[mid]) {
+                high = mid-1;
+            }
+            else
+                low = mid+1;
+        }
+        return 0;
+
+    }
+
+    static void printResult(int index) {
+
+        if(index!=0) {
+            System.out.println("Key " + key + " is found in index " + index);
+        }
+        else {
+            System.out.println("Key not found!");
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] inputArray = new int[]{2,5,8,9,10,13,17};
+
+        key = 17;
+        index = BinarySearch(inputArray, inputArray.length, key);
+        printResult(index);
+
+        key = 90;
+        index = BinarySearch(inputArray, inputArray.length, key);
+        printResult(index);
+    }
+}
+
